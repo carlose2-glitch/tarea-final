354a595b92e5bc5d4e36adf37ca6de94aba92fb8
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-07-27 23:28:36 -0400

    first commit

---
+++ curriculo.js
@@ -0,0 +1,62 @@
+const body = document.querySelector('body');
+
+
+body.innerHTML = ` <main class="principal">
+
+        <header>
+            <h1>Carlos Eduardo Sanchez Carrasco</h1>
+        </header>
+
+          <aside>
+            <article>
+                <img src="./img/WhatsApp_Image_2024-07-27_at_7.15.26_PM-removebg-preview.png" alt="img">
+            </article>
+          
+            <article>
+                <i class="fa-solid fa-house" style="color: #232325;"></i>
+                <p>La Yaguara, Colinas de vista alegre, URB los Caciques</p>
+            </article>
+
+            <article>
+                <i class="fa-solid fa-phone" style="color: #2c2a32;"></i>
+                <p>0414-204-57-86</p>
+            </article>
+            
+           <article>
+            <i class="fa-solid fa-envelope" style="color: #242629;"></i>
+            <p>carloseduardosanchezcarrasco@gmail.com</p>
+           </article>
+           
+           <article>
+            <i class="fa-regular fa-address-card" style="color: #34373d;"></i>
+            <p>26.227.179 - 27 años</p>
+           </article>
+            
+          </aside>
+
+          <main class="secundario">
+            <h2>Experiencia laboral</h2>
+            <article>
+                <p>Periodo: 2015-2019</p>
+                <p>Cargo: asistente administrativo</p>
+                <p>Empresa: Aliado ZOOM La Yaguara</p>
+            </article>
+
+            <article>
+                <p>Periodo: 2019-2023</p>
+                <p>Cargo: Operador</p>
+                <p>Empresa: Exportadora C.I Metales la Unión</p>
+            </article>
+            <h2>Estudios Realizados</h2>
+            <article>
+                <p>Periodo: 2010-2015</p>
+                <p>Tecnico medio en instrumentacion Industrial</p>
+                <p>Escuela Tecnica Julio Calcaño</p>
+            </article>
+
+          </main>
+          <footer>
+            <p>Nota: Actualmente soy programador auto didacta <a href="https://github.com/carlose2-glitch" target="_blank">GITHUB</a> y estudio programacion en Uneweb Instituto de Comunicación Web C.A J-29431337-0</p>
+          </footer>
+
+    </main>`;
